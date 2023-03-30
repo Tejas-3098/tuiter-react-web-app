99f82c18fc1388a051379ea62953cd5a10447f46
Tejas <tejas3098@gmail.com>
2023-03-30 05:12:16 -0400

    a7 final changes

--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -1,7 +1,7 @@
 import React from "react";
 const WhoToFollowListItem = (
     {
-        who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'NASA.jpg'}
+        who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'NASA.jpeg'}
     }
 ) => {
     return(
